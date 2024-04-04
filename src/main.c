0734209720f677ae9d3a487f2a278588320b3337
mikastiv <mikastiv@outlook.com>
2024-04-04 13:14:29 -0400

    lookup ip and hostname

--- src/main.c
+++ src/main.c
@@ -1,13 +1,22 @@
 #include <arpa/inet.h>
+#include <ctype.h>
 #include <errno.h>
 #include <netdb.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 static const char* progname;
+
+typedef struct {
+    const char* dst;
+    char ip[32];
+    char host[NI_MAXHOST];
+    struct sockaddr_in addr;
+} PingData;
 
 void
 print_error(const char* restrict fmt, ...) {
@@ -22,55 +31,94 @@ usage() {
     print_error("usage: %s [options] <destination>\n", progname);
 }
 
-int
-main(int argc, char* const* argv) {
-    if (argc > 0) progname = argv[0];
-    if (argc < 2) {
-        usage();
-        exit(EXIT_FAILURE);
+bool
+is_ipv4(const char* str) {
+    size_t dots = 0;
+    for (size_t i = 0; str[i]; i++) {
+        if (!isdigit(str[i]) && str[i] != '.') {
+            return false;
+        }
+        if (str[i] == '.') {
+            dots += 1;
+        }
     }
 
-    struct sockaddr_in dst;
-    dst.sin_family = AF_INET;
-    dst.sin_port = 0;
+    if (dots > 3) {
+        return false;
+    }
 
-    struct addrinfo hints = {
-        .ai_family = AF_INET,
-        .ai_socktype = SOCK_RAW,
-        .ai_protocol = IPPROTO_ICMP,
-    };
-    struct addrinfo* result = NULL;
+    return true;
+}
 
-    int res = getaddrinfo(argv[1], NULL, &hints, &result);
+struct sockaddr_in
+lookup_addr(const char* dst) {
+    struct sockaddr_in out;
+
+    if (is_ipv4(dst)) {
+        switch (inet_pton(AF_INET, dst, &out.sin_addr.s_addr)) {
+            case 0: {
+                print_error("not in presentation format\n");
+                exit(EXIT_FAILURE);
+                break;
+            }
+            case -1: {
+                const char* err = strerror(errno);
+                print_error("%s: %s\n", progname, err);
+                exit(EXIT_FAILURE);
+                break;
+            }
+            default:
+                break;
+        }
+    } else {
+        struct addrinfo hints = {
+            .ai_family = AF_INET,
+            .ai_socktype = SOCK_RAW,
+            .ai_protocol = IPPROTO_ICMP,
+        };
+        struct addrinfo* result = NULL;
+
+        const int res = getaddrinfo(dst, NULL, &hints, &result);
+        if (res != 0) {
+            const char* err = gai_strerror(res);
+            print_error("%s: %s\n", progname, err);
+            exit(EXIT_FAILURE);
+        }
+
+        out = *(struct sockaddr_in*)result->ai_addr;
+        freeaddrinfo(result);
+    }
+
+    return out;
+}
+
+void
+lookup_hostname(struct sockaddr_in addr, char* buffer, const size_t buffer_size) {
+    const size_t addrlen = sizeof(struct sockaddr_in);
+    const int res = getnameinfo((struct sockaddr*)&addr, addrlen, buffer, buffer_size, NULL, 0, NI_NAMEREQD);
     if (res != 0) {
         const char* err = gai_strerror(res);
         print_error("%s: %s\n", progname, err);
         exit(EXIT_FAILURE);
     }
+}
 
-    char host[NI_MAXHOST];
-
-    res = getnameinfo(result->ai_addr, sizeof(struct sockaddr_in), host, sizeof(host), NULL, 0, NI_NAMEREQD);
-
-    struct sockaddr_in addr = *(struct sockaddr_in*)result->ai_addr;
-    char ip[NI_MAXSERV];
-    const char* ip_presentable = inet_ntop(AF_INET, &addr.sin_addr.s_addr, ip, INET_ADDRSTRLEN);
-    printf("host: %s\n", host);
-    printf("ip: %s\n", ip_presentable);
-
-    switch (inet_pton(AF_INET, argv[1], &dst.sin_addr.s_addr)) {
-        case 0: {
-            print_error("not in presentation format\n");
-            exit(EXIT_FAILURE);
-            break;
-        }
-        case -1: {
-            const char* err = strerror(errno);
-            print_error("%s: %s\n", progname, err);
-            exit(EXIT_FAILURE);
-            break;
-        }
-        default:
-            break;
+int
+main(int argc, char* const* argv) {
+    progname = argc > 0 ? argv[0] : "ft_ping";
+    if (argc < 2) {
+        usage();
+        exit(EXIT_FAILURE);
     }
+
+    PingData ping = {
+        .dst = argv[argc - 1],
+    };
+
+    ping.addr = lookup_addr(ping.dst);
+    lookup_hostname(ping.addr, ping.host, sizeof(ping.host));
+    inet_ntop(AF_INET, &ping.addr.sin_addr.s_addr, ping.ip, INET_ADDRSTRLEN);
+
+    printf("host: %s\n", ping.host);
+    printf("ip: %s\n", ping.ip);
 }
